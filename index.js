e4ab130c503709c982bae53adfd06fe3e4887a98
0HPKaramazov <simone.assispereira1973@gmail.com>
2024-12-09 04:19:02 -0300

    x X

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Up and running...');
+    res.status(200).send('Up and running...');
 });
 
 app.use('/crd', cardRoutes);
